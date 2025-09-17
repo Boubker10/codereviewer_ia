2439710145d6e6d067dbf5d13756cbf9506a8a40
Boubker BENNANI <boubker.bennani@etu.imt-nord-europe.fr>
2025-09-17 19:05:18 +0000

    fix worflows

--- src/core/git_client.py
+++ src/core/git_client.py
@@ -3,12 +3,21 @@ import requests
 
 
 class GitClient:
-    def __init__(self, repo: str, token: str | None = None):
+    def __init__(self, repo: str | None = None, token: str | None = None):
+        if not repo:
+            owner = os.getenv("GIT_USERNAME")
+            name = os.getenv("GIT_repo")
+            if not owner or not name:
+                raise ValueError(
+                    "GITHUB_REPO_OWNER ou GITHUB_REPO_NAME n'est pas défini dans les variables d'environnement."
+                )
+            repo = f"{owner}/{name}"
+
         self.repo = repo
-        self.token = token or os.getenv("GIT_TOKEN")
+        self.token = token or os.getenv("GITHUB_TOKEN") or os.getenv("GIT_TOKEN")
         if not self.token:
-            raise ValueError("Aucun token GitHub fourni. Définis GIT_TOKEN dans ton .env")
-        
+            raise ValueError("Aucun token GitHub fourni. Définis GITHUB_TOKEN dans ton .env ou secrets.")
+
         self.base_url = f"https://api.github.com/repos/{repo}"
         self.headers = {
             "Authorization": f"token {self.token}",
@@ -22,7 +31,6 @@ class GitClient:
         return response.json()
 
     def get_pull_request_info(self, pr_number: int):
-
         url = f"{self.base_url}/pulls/{pr_number}"
         response = requests.get(url, headers=self.headers)
         response.raise_for_status()
@@ -40,4 +48,3 @@ class GitClient:
         response = requests.get(url, headers=self.headers)
         response.raise_for_status()
         return response.json()
-    
