b5d3c422f639385aaa6dc0e1f32e0300be00f82e
Boubker10 <boubker.bennani@etu.imt-nord-europe.fr>
2025-09-17 15:33:18 +0200

    adjust readme.md

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ load_dotenv()
 
 @bot.event
 async def on_ready():
-    print(f"✅ Bot connecté en tant que {bot.user}")
+    print(f"sBot connecté en tant que {bot.user}")
 
     repo_name = f"{os.getenv('GITHUB_USERNAME')}/repo_test_codereviewer"
     github_token = os.getenv("GITHUB_TOKEN")
     reviewer = Reviewer(repo_name, github_token)
 
-    print("🔍 Analyse de toutes les PR ouvertes...")
+    print("Analyse de toutes les PR ouvertes...")
     await reviewer.analyze_all_open_prs(post_to_github=True, post_to_discord=True)
     # await reviewer.analyze_pr(pr_number=3, post_to_github=True, post_to_discord=True)
     print("💡 Reviews postées. Le bot reste actif pour les prochaines PR.")
